901bd05a212ce68463c5bc7574f1922485bab611
vaishnaviburadkar <parimal08022001@gmail.com>
2021-04-23 10:33:26 +0000

    Adding a Test Script

---
+++ webserver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum install -y httpd
+echo "<h1>This webpage has been built with the help of Jenkins server </h1>" > /var/www/html/index.html
+systemctl start httpd
+systemctl status httpd
+sleep 10
+
